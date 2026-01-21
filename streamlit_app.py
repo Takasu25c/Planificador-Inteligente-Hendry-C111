2bdb812ecd2cfc50e5d6942d6f5e4f44cf0bb5a6
Takasu25c <takasu25c@gmail.com>
2026-01-21 10:30:24 -0500

    general update 0.21126
    Se le agrega fondo a la aplicacion, se crea el archivo .json, Se modifica la lista de eventos para diferenciar los mantenimientos, se mejora la interfaz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 import Recursos_y_Personal as rh
 import eventos as ev
+import datetime as dt
+"""import base64
+def get_base64(bin_file): 
+    with open(bin_file, 'rb') as f: 
+        data = f.read() 
+    return base64.b64encode(data).decode() 
+def set_background(jpg_file): 
+    bin_str = get_base64(jpg_file) 
+    page_bg_img = f""" """
+    <style> 
+    [data-testid="stAppViewContainer"] {{ 
+        background-image: url("data:image/jpg;base64,{bin_str}");
+        background-size: cover; 
+        background-repeat: no-repeat; 
+        background-attachment: fixed;
+          }} 
+          </style> 
+    st.markdown(page_bg_img, unsafe_allow_html=True) # Llamar a la función con tu imagen 
+set_background("background.jpg")"""
+recursos_en_uso = []
 st.title("TERAX")
 st.header("Reparacion de Equipos de Computo")
-st.selectbox("Seleccione el tipo de reparacion",ev.lista_de_eventos)
+evento = st.selectbox("Seleccione el tipo de reparacion",ev.lista_de_eventos)
+if evento == "Defectacion":
+    recurso =(st.selectbox("Selecione los recursos a usar",rh.listarecursos))
+    boton = st.button("Agregar Recurso")
+    if boton:
+        recursos_en_uso.append(recurso)
+        st.success("Recurso Agregado")
+    
+    momento_inicio = st.date_input("Seleccione la fecha",dt.date.today())
+    hora_inicio = st.time_input("Seleccione la hora",dt.time(12,0))
